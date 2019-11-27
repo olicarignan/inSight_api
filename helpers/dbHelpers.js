6c24867392fcaf3d0c3f0ab3cf6de6600a8e6bff
Karen <karendavtyan21@mail.com>
2019-11-27 22:51:01 +0000

    added note post request

--- helpers/dbHelpers.js
+++ helpers/dbHelpers.js
@@ -1,45 +1,50 @@
 module.exports = knex => {
 
   const getUsers = () => {
-
     return knex
       .select('*')
       .from('users')
   }
 
   const getCategories = () => {
-
     return knex
       .select('*')
-      .from('categories')
-      
+      .from('categories')   
   }
-  const getAppointments = () => {
 
+  const getAppointments = () => {
     return knex
       .select('*')
       .from('appointments')
   }
-  const getNotes = () => {
 
+  const getNotes = () => {
     return knex
       .select('*')
       .from('notes')
+  }
+
+  const addNote = (note) => {
+    return knex('notes')
+      .insert({category_id: note.category_id, appointment_id: note.appointment_id, note_title: note.note_title, note_content: note.note_content, note_preview: note.note_preview})
+      .returning('*')
+      .then(res => res.rows[0])
   }
 
   const addUser = (user) => {
     return knex('users')
       .insert({first_name: user.first_name, last_name: user.last_name, email: user.email, password: user.password})
       .returning('*')
-      .then(res => console.log(res.rows[0]))
+      .then(res => res.rows[0])
   }
-  const getUsersLogin = (email) => {
 
+  const getUsersLogin = (email) => {
     return knex
       .select('*')
       .from('users')
       .where({email: email})
   }
+
   const addAppointment = (appointment) => {
     return knex('appointments')
       .insert({appointment_name: appointment.appointment_name, location: appointment.location, start_time: appointment.start_time, end_time: appointment.end_time, category_id: appointment.category_id, user_id: appointment.user_id})
@@ -56,6 +61,7 @@ module.exports = knex => {
     getNotes,
     addUser,
     getUsersLogin,
-    addAppointment
+    addAppointment,
+    addNote
   }
 }
