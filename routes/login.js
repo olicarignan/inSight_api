cf33be6e7baabd3bdd2654cab9afacc7bc0caca6
olicarignan <olive.carignan@gmail.com>
2019-11-27 01:41:03 +0000

    register

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getApiResults } = require('../helpers/apiHelpers');
 
 //get users route
-module.exports = ({getUsers, addUser}) => {
+module.exports = ({getUsersLogin, addUser}) => {
   router.post('/', (req, res, next) => {
     getUsersLogin(req.body.email, req.body.password)
     .then(result => {
