0d7a87f22768c7c993292a4075efb7dde9bb1319
olicarignan <olive.carignan@gmail.com>
2019-11-28 19:48:46 +0000

    handle register request

--- routes/register.js
+++ routes/register.js
@@ -1,33 +1,29 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const router = express.Router();
-const { getApiResults } = require('../helpers/apiHelpers');
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken');
+const { getApiResults } = require("../helpers/apiHelpers");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports = ({ addUser }) => {
-  router.post('/', (req, res, next) => {
-    console.log(req.body.user)
-    const user = req.body.user
-    user.password = bcrypt.hashSync(user.password, 10)
+  router.post("/", (req, res, next) => {
+    console.log(req.body);
+    const user = req.body;
+    user.password = bcrypt.hashSync(user.password, 10);
     addUser(user)
       .then(user => {
-        const newUser = user[0];
-        console.log(newUser)
-        if(!newUser) {
-          throw new Error('error')
+        console.log(user);
+        if (!user) {
+          throw new Error("error");
         }
-        jwt.sign(newUser, process.env.ACCESS_TOKEN_SECRET, (err, token) => {
+        jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, (err, token) => {
           // window.localStorage.setItem({token})
           // res.send({token})
-          res.json({newUser, token})
-          .status(204);
+          res.json({ user, token }).status(204);
         });
         // res.json(user[0]);
       })
       .catch(e => res.send(e));
-  })
-
-  
+  });
   return router;
 };
