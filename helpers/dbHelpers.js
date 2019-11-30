363f9686fed73f2d042e5f0fe096e8bfc99debd6
nasouh94 <nasouh@live.com>
2019-11-30 00:44:46 +0000

    fixed syntax errors

--- helpers/dbHelpers.js
+++ helpers/dbHelpers.js
@@ -1,140 +1,128 @@
-module.exports = knex => {
+module.exports = (knex) => {
+	const getUsers = () => {
+		return knex.select('*').from('users');
+	};
 
-  const getUsers = () => {
-    return knex
-      .select('*')
-      .from('users')
-  }
+	const getCategories = (user_id) => {
+		console.log(user_id);
+		return knex.select('*').from('categories').where({ user_id: user_id });
+	};
+	const addCategories = (category) => {
+		return knex('categories')
+			.insert({ user_id: category.user_id, category_name: category.category_name, colour: category.colour })
+			.returning('*');
+	};
 
-  const getCategories = (user_id) => {
-    console.log(user_id)
-    return knex
-      .select('*')
-      .from('categories')
-      .where({user_id: user_id})
-      
-  }
-  const addCategories = (category) => {
-    return knex('categories')
-      .insert({user_id: category.user_id, category_name: category.category_name, colour: category.colour})
-      .returning('*')
-  }
+	const getAppointments = (user_id) => {
+		return knex.select('*').from('appointments').where({ user_id: user_id });
+	};
 
-  const getAppointments = (user_id) => {
-    return knex
-      .select('*')
-      .from('appointments')
-      .where({user_id: user_id})
+	const updateCategory = (category) => {
+		return knex('categories')
+			.where({ id: category.id })
+			.update({ category_name: category.category_name, colour: category.colour });
+	};
 
-  
-  const updateCategory = (category) => {
-    return knex('categories')
-      .where({id: category.id})
-      .update({ category_name: category.category_name, colour: category.colour})
-  }
-  
-  const deleteCategory = (category) => {
-    return knex('categories')
-      .where({id: category.id})
-      .del()
+	const deleteCategory = (category) => {
+		return knex('categories').where({ id: category.id }).del();
+	};
 
-  }
+	const getNotes = () => {
+		return knex.select('*').from('notes');
+	};
 
-  const getNotes = () => {
-    return knex
-      .select('*')
-      .from('notes')
-  }
+	const addNote = (note) => {
+		return knex('notes')
+			.insert({
+				category_id: note.category_id,
+				appointment_id: note.appointment_id,
+				note_title: note.note_title,
+				note_content: note.note_content,
+				note_preview: note.note_preview
+			})
+			.returning('*')
+			.then((res) => res.rows[0]);
+	};
 
-  const addNote = (note) => {
-    return knex('notes')
-      .insert({category_id: note.category_id, appointment_id: note.appointment_id, note_title: note.note_title, note_content: note.note_content, note_preview: note.note_preview})
-      .returning('*')
-      .then(res => res.rows[0])
-  }
+	const updateNote = (note) => {
+		return knex('notes')
+			.where({ id: note.id })
+			.update({
+				category_id: note.category_id,
+				appointment_id: note.appointment_id,
+				note_title: note.note_title,
+				note_content: note.note_content,
+				note_preview: note.note_preview
+			});
+	};
 
-  const updateNote = (note) => {
-    return knex('notes')
-      .where({id: note.id})
-      .update({category_id: note.category_id, appointment_id: note.appointment_id, note_title: note.note_title, note_content: note.note_content, note_preview: note.note_preview})
-  }
+	const deleteNote = (note) => {
+		return knex('notes').where({ id: note.id }).del();
+	};
 
-  const deleteNote = (note) => {
-    return knex('notes')
-      .where({id: note.id})
-      .del()
-  }
+	const addUser = (user) => {
+		return knex('users')
+			.insert({
+				first_name: user.first_name,
+				last_name: user.last_name,
+				email: user.email,
+				password: user.password
+			})
+			.returning('*');
+	};
 
-  const addUser = (user) => {
-    return knex('users')
-      .insert({first_name: user.first_name, last_name: user.last_name, email: user.email, password: user.password})
-      .returning('*')
-  }
-
-  const getUsersLogin = (email) => {
-    return knex
-      .select('*')
-      .from('users')
-      .where({email: email})
-  }
-
-  const getAppointments = () => {
-    return knex
-      .select('*')
-      .from('appointments')
-  }
-
-  const updateAppointment = (appointment) => {
-    return knex('appointments')
-      .where({id: appointment.id})
-      .update({start_date: appointment.start_date, 
-        start_time: appointment.start_time,
-        end_date: appointment.end_date, 
-        end_time: appointment.end_time,
-        appointment_name: appointment.appointment_name,
-        category_id: appointment.category_id,
-        location: appointment.location,
-        appointment_small_note: appointment.appointment_small_note})
-  }
-  const addAppointment = (appointment) => {
-    return knex('appointments')
-      .insert({start_date: appointment.start_date, 
-               start_time: appointment.start_time,
-               end_date: appointment.end_date, 
-               end_time: appointment.end_time,
-               appointment_name: appointment.appointment_name,
-               category_id: appointment.category_id,
-               location: appointment.location,
-               user_id: appointment.user_id,
-               appointment_small_note: appointment.appointment_small_note
-               })
-      .returning('*')
-  }
-
-  const deleteAppointment = (appointment) => {
-    return knex('appointments')
-      .where({id: appointment.id})
-      .del()
-  }
+	const getUsersLogin = (email) => {
+		return knex.select('*').from('users').where({ email: email });
+	};
 
 
+	const updateAppointment = (appointment) => {
+		return knex('appointments').where({ id: appointment.id }).update({
+			start_date: appointment.start_date,
+			start_time: appointment.start_time,
+			end_date: appointment.end_date,
+			end_time: appointment.end_time,
+			appointment_name: appointment.appointment_name,
+			category_id: appointment.category_id,
+			location: appointment.location,
+			appointment_small_note: appointment.appointment_small_note
+		});
+	};
+	const addAppointment = (appointment) => {
+		return knex('appointments')
+			.insert({
+				start_date: appointment.start_date,
+				start_time: appointment.start_time,
+				end_date: appointment.end_date,
+				end_time: appointment.end_time,
+				appointment_name: appointment.appointment_name,
+				category_id: appointment.category_id,
+				location: appointment.location,
+				user_id: appointment.user_id,
+				appointment_small_note: appointment.appointment_small_note
+			})
+			.returning('*');
+	};
 
+	const deleteAppointment = (appointment) => {
+		return knex('appointments').where({ id: appointment.id }).del();
+	};
 
-  return {
-    getUsers,
-    getCategories,
-    getAppointments,
-    getNotes,
-    addUser,
-    getUsersLogin,
-    addAppointment,
-    addNote,
-    addCategories,
-    updateAppointment,
-    updateCategory,
-    updateNote,
-    deleteAppointment,
-    deleteCategory,
-    deleteNote
-  }
-}
+	return {
+		getUsers,
+		getCategories,
+		getAppointments,
+		getNotes,
+		addUser,
+		getUsersLogin,
+		addAppointment,
+		addNote,
+		addCategories,
+		updateAppointment,
+		updateCategory,
+		updateNote,
+		deleteAppointment,
+		deleteCategory,
+		deleteNote
+	};
+};
